cbafd1c6fd97a4f72d80d320e80facd99bf115bb
Joshua Cook <joshuacook13@gmail.com>
2013-06-09 00:54:49 -0400

    Changes to footer - social icons spin

--- wp-content/themes/alexis_theme/footer.php
+++ wp-content/themes/alexis_theme/footer.php
@@ -5,23 +5,62 @@
                         
                         <div class="large-6 columns small-content-centered text-left">
                             <!-- <img alt="Connect via LinkedIn" src="<?php echo get_template_directory_uri() . '/img/assets/icons/linkedin-circle-white-32x32.png'; ?>" /> -->
-                            <a class="social-icon" href="#">
-                                <div class="circle">
-                                    <i class="icon-linkedin-rect animate-spin"></i>
+                            <div class="poster">
+                                <div class="movement">
+                                    <div class="face front">
+                                        <a class="social-icon" href="#">
+                                            <div class="circle">
+                                                <i class="icon-linkedin-rect"></i>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div class="face back">
+                                        <a class="social-icon" href="#">
+                                            <div class="circle">
+                                                <i class="icon-linkedin-rect"></i>
+                                            </div>
+                                        </a>
+                                    </div>
                                 </div>
-                            </a>
-                            <a class="social-icon" href="#">
-                                <div class="circle">
-                                    <i class="icon-facebook-1"></i>
+                            </div>
+                            <div class="poster">
+                                <div class="movement">
+                                    <div class="face front">
+                                        <a class="social-icon" href="#">
+                                            <div class="circle">
+                                                <i class="icon-facebook-1"></i>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div class="face back">
+                                        <a class="social-icon" href="#">
+                                            <div class="circle">
+                                                <i class="icon-facebook-1"></i>
+                                            </div>
+                                        </a>
+                                    </div>
                                 </div>
-                            </a>
-                            <a class="social-icon" href="#">
-                                <div class="circle">
-                                    <i class="icon-instagram-filled"></i>
+                            </div>
+                            <div class="poster">
+                                <div class="movement">
+                                    <div class="face front">
+                                        <a class="social-icon" href="#">
+                                            <div class="circle">
+                                                <i class="icon-instagram-filled"></i>
+                                            </div>
+                                        </a>  
+                                    </div>
+                                    <div class="face back">
+                                        <a class="social-icon" href="#">
+                                            <div class="circle">
+                                                <i class="icon-instagram-filled"></i>
+                                            </div>
+                                        </a>  
+                                    </div>
                                 </div>
-                            </a>                            
-                        </div>
-                        
+                            </div>
+                              
+                        </div>                      
                         <div class="large-6 columns hide-for-small copyright crimson text-right">
                             Copyright &copy; <?php echo date('Y'); ?>, Alexis Contreras
                         </div>
