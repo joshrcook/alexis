684c40c90d01f38f08b60b0266a83e55c5ecf82e
Joshua Cook <joshuacook13@gmail.com>
2013-07-12 14:51:59 -0400

    Minor style fix on singe jrc_por template

--- wp-content/themes/alexis_theme/category.php
+++ wp-content/themes/alexis_theme/category.php
@@ -1,1 +1,18 @@
-<p>This is the category template</p>
+<?php get_header(); ?>
+
+<div class="row">
+	<div class="columns large-9 small-12">
+
+	<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+			
+			<?php get_template_part( 'content', 'post-excerpt' ); ?>
+
+	<?php endwhile; endif; ?>
+
+	</div><!-- end body column -->
+	<div class="columns large-3 small-12">
+		<?php get_sidebar('blog'); ?>
+	</div>
+</div><!-- end first row -->
+
+<?php get_footer(); ?>
